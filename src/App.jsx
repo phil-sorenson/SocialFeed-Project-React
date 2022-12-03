9e975abe3db0e92cf57b51941ea8a6e15c635798
phil-sorenson <phillip.sorenson31@gmail.com>
2022-12-03 01:22:15 -0600

    Installed npx program for React and created my Components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+function App() {
+
+  const [entries, setEntries] = useState()
+  
+  return (
+    <div>
+      <h3>Hello World!</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CreatePosts/CreatePosts.jsx
@@ -0,0 +1,1 @@
+//* CreatePost == AddEntryForm (weight-tracker)
